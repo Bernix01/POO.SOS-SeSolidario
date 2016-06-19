f1d5fec785c11a53b643d7c6deb1d2c6b76fd439
whugo43 <whugo43@hotmail.com>
2016-06-19 12:58:49 -0500

    sesion exitosa

--- src/sos/SOS.java
+++ src/sos/SOS.java
@@ -131,7 +131,7 @@ public class SOS {
                         System.out.println(" ");
                     }
                 }
-                int b=mostrarMenuUsuario();
+              mostrarMenuUsuario();
             }
         }
 
